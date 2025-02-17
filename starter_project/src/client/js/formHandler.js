529df461c03ce98296170664707238d71b8a117a
Mariam Hasanat <hasanattmariam@gmail.com>
2025-02-17 18:26:42 +0200

    fix the functionality and use library Client

--- starter_project/src/client/js/formHandler.js
+++ starter_project/src/client/js/formHandler.js
@@ -1,30 +1,49 @@
-// Replace checkForName with a function that checks the URL
-import { checkForName } from './nameChecker'
+// // Replace checkForName with a function that checks the URL
+// import { checkForName } from './nameChecker'
 
-// If working on Udacity workspace, update this with the Server API URL e.g. `https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api`
-// const serverURL = 'https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api'
-const serverURL = 'https://localhost:8000/api'
+// // If working on Udacity workspace, update this with the Server API URL e.g. `https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api`
+// // const serverURL = 'https://wfkdhyvtzx.prod.udacity-student-workspaces.com/api'
+// const serverURL = 'https://localhost:8000/api'
 
-const form = document.getElementById('urlForm');
-form.addEventListener('submit', handleSubmit);
+// const form = document.getElementById('urlForm');
+// form.addEventListener('submit', handleSubmit);
+
+// function handleSubmit(event) {
+//     event.preventDefault();
+
+//     // Get the URL from the input field
+//     const formText = document.getElementById('name').value;
+
+//     // This is an example code that checks the submitted name. You may remove it from your code
+//     Client.checkForName(formText)
+    
+//     // Check if the URL is valid
+ 
+//         // If the URL is valid, send it to the server using the serverURL constant above
+      
+// }
+
+// // Function to send data to the server
+
+// // Export the handleSubmit function
+// export { handleSubmit };
 
 function handleSubmit(event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    // Get the URL from the input field
-    const formText = document.getElementById('name').value;
+    // check what text was put into the form field
+    let formText = document.getElementById('name').value
 
-    // This is an example code that checks the submitted name. You may remove it from your code
-    checkForName(formText);
-    
-    // Check if the URL is valid
- 
-        // If the URL is valid, send it to the server using the serverURL constant above
-      
+    Client.checkForName(formText)
+
+    console.log("::: Form Submitted :::")
+    fetch('http://localhost:8081/test')
+    .then(res => {
+        return res.json()
+    })
+    .then(function(data) {
+        document.getElementById('results').innerHTML = data.message
+    })
 }
 
-// Function to send data to the server
-
-// Export the handleSubmit function
-export { handleSubmit };
-
+export { handleSubmit }
--- starter_project/webpack.prod.js
+++ starter_project/webpack.prod.js
@@ -6,6 +6,10 @@ const WorkboxPlugin = require('workbox-webpack-plugin');
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     module: {
         rules: [
             {
@@ -16,7 +20,7 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-        }
+            }
         ]
     },
     plugins: [
